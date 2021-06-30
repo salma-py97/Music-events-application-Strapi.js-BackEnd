9e462b6f76ca6f66630495649fcc0c12795d5043
Salma EL BARMAKI <elbarmaki.salma@gmail.com>
2021-06-30 03:18:59 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6fa5f49a9c40ccc0c897196f400642fe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f1bfd1e4-ee36-4e0b-a5b5-dadaf83962dd'
+};
